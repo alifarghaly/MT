01ca9107ecdc568a7270e2342ff320053e9d9e12
Ali Farghaly <alifarghaly@yahoo.com>
2021-10-25 21:12:50 -0700

    Add files via upload
    MT1  has the code 
    dict has a python dictionary of Arabic fixed phrases and their English translations

---
+++ dict.py
@@ -0,0 +1,12 @@
+dict = {
+        'good morning': 'صباح الخير',
+        'good evening' :  'مساء الخير',
+        'welcome': 'مرحبا',
+        'how are you': 'كيف حالك',
+        'how you doing?': 'كيفك',
+        'goodbye' : 'مع السلامة',
+        'goodbye' : 'في امان الله',
+        
+        'how are you' : 'ايش لونك'
+        
+          }
