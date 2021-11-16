f7b964db2ea5371aaa4ebf5204b1d3e15357e81c
Ali Farghaly <alifarghaly@yahoo.com>
2021-11-16 13:23:15 -0800

    Add files via upload
    Added voice text to speech in the translation

--- mt1.py
+++ mt1.py
@@ -2,6 +2,13 @@
 from dict import dict
 # Importing the REGEX module which is needed for normalizing Arabic texts
 import re
+import os
+# Import the required module for text  to speech conversion and speech to text
+from gtts import gTTS
+from playsound import playsound
+import speech_recognition as sr
+ 
+
 # lists of Arabic and English letters to identfy the language of input text
 English = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 Arabic = ['ش', 'غ', 'ظ', 'ذ', 'خ', 'ث', 'ت', 'س', 'ر', 'ق', 'ض', 'ف', 'ع', 'ص', 'ن', 'م', 'ل', 'ك', 'ي', 'ط', 'ح', 'ز', 'و', 'ه', 'د', 'ج', 'ب', 'ا']
@@ -32,22 +39,46 @@ def translate():
         translate_en(text)
     if ar >= .5:
         translate_ar(text)
-# This is the function that gets an English text and outputs the Arabic translation
+# This is the function that gets an English text and outputs the Arabic translation spoken and written
 def translate_en(text):
     text = text.lower()
     if text in dict.keys():
+# converting text into acoustic signal (speech)
+        myobj = gTTS(text=(dict[text]), lang='ar', slow=False)
+        myobj.save('sound1.mp3')
+# playing the acoustic signal of the English translation 
+        playsound('sound1.mp3')
         print(dict[text])
     else:
         print("Sorry, your phrase is not in my data base")
+    print("would you like to continue?, type 'y' for yes or 'Ctrl-D' to quit")
+    response = input()
+    if response == 'y':
+        translate()
+    else:
+        os.abort()
 # This is the function that takes an Arabic text and gives the English translation 
-
+import pyttsx3
 def translate_ar(text):
     
     #text= re.sub('أ'|'إ', 'ا', text)
     for key, value in dict.items():
         if text == value:
+# converting text into acoustic signal (speech)
+            engine = pyttsx3.init()
+# playing the acoustic signal of the English translation
+            engine.say(key)
+            engine.runAndWait()
             print(key)
             if text != value:
                 print("Sorry, your phrase is not in my data base")
+                print(' type "y" to continue or "CTRL-D"   to close the programn')
+    response = input()
+    if response == 'y':
+        translate()
+    else:
+        os.abort()
+        
+    
 translate()    
    
